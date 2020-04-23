5dccded249ffeb04153a24e9b0930ddaa76a113f
TheBoilerman <57120760+TheBoilerman@users.noreply.github.com>
2020-04-23 15:16:15 -0700

    Framework sketch

--- blackjack.py
+++ blackjack.py
@@ -54,9 +54,24 @@ def analyzeHand(hand):
 def main():
 
     # Make deck, shuffle it
-    # Ask how many players there are and their names
-    # Make that many players
+    # Get player name
+    # Make player and dealer
+    # Make player bank
+    # Deal player hand, deal dealer's hand
+    # Show one of dealer's cards
 
+    # Check player's hand for blackjack
+        # If blackjack, return bet + bet*1.5
+    # If not blackjack, ask to hit or stand
+    # If stand, break to dealer
+    # If hit, deal one card to player, check score of hand
+        # If score is over 21, player loses bet
+        # If score = 21, player stands
+    # If score is less than 21, repeat
+
+    # When player stands, dealer checks hand
+    # If hand is greater than player hand, player loses bet
+    # Else if hand is less than 17, dealer hits
 
     printGreeting()
 
@@ -64,9 +79,13 @@ def main():
     deck.shuffle()
 
     player = Player('Paul')
+    dealer = Player('dealer')
+
+    dealer.getHand(deck, 2)
     player.getHand(deck, 2)
 
     print(f"{player.name}'s hand: {player.hand}")
+    print(f"{dealer.name}'s hand: {dealer.hand[0]}")
     
     while True:
         if score > 21:
