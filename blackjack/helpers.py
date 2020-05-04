ed6905b84f2e2caf072610ae74fd79af7a7ffc33
Paul Carroll <57120760+carrollpaul@users.noreply.github.com>
2020-05-04 11:32:03 -0700

    Moving functions to helper.py

--- blackjack/helpers.py
+++ blackjack/helpers.py
@@ -51,3 +51,111 @@ class Player:
     def getHand(self, deck, size): # Deal hand equal to given size
         for i in range(size):
             self.hand.append(deck.dealCard())
+
+def printGreeting():
+    print("\n*************************\n\nWelcome to Paul's Casino!\n\n*************************\n")
+
+def getBet(player): # Get bet from player
+    while True: 
+        try: # Make sure bet is a number
+            bet = int(input('Input bet: '))
+        except:
+            print('Enter numeric bet please.')
+            continue
+        if (player.bank - bet > 0): # Player can't bet more than they have
+            return bet
+        else: 
+            print(f"You can't bet what you don't have! Your bankroll is ${player.bank}.")
+            continue
+
+def analyzeHand(hand): # Get value of hand
+    score = 0
+    ace = False
+    for card in hand:
+        rank = card.rank
+        if (rank == 'A'):
+            score += 1
+            ace = True
+        elif (rank == 'K' or rank == 'Q' or rank == 'J'):
+            score += 10
+        else:
+            score += int(rank)
+    if not ace:
+        return score
+    elif score < 11: # If a hand containing one or more aces valued at 1 is less than 11, change value of one ace to 11 (add 10)
+        score += 10
+        return score 
+    else:
+        return score
+
+def playHand(player, dealer, deck): # Function for a single hand of blackjack
+    bet = getBet(player)
+
+    dealer.getHand(deck, 2) # Deal 2 cards to player and dealer
+    player.getHand(deck, 2)
+
+    print(f"{player.name}'s hand: {player.hand}") # Print player's hand and 1 card of dealer's hand
+    print(f"{dealer.name}'s hand: {dealer.hand[0]}")
+
+    # Check for blackjack
+    playerScore = analyzeHand(player.hand)
+    if (playerScore == 21):
+        print('Blackjack!')
+        player.bank = player.bank + bet*1.5
+        return
+    
+    # Loop for player
+    while True:
+        while True:
+            hit = input('Hit? (y/n): ') # See if player wants to hit or stand
+            # Make sure input is y or n
+            if hit.upper() == 'Y' or hit.upper() == 'N': 
+                break
+            else:
+                print("Please enter 'y' or 'n'.")
+                continue
+
+        if (hit.upper() == 'N'):
+            playerScore = analyzeHand(player.hand)
+            break
+        else:
+            player.getHand(deck, 1) # If hit, deal one card and print new hand, then analyze hand
+            print(f"{player.name}'s hand: {player.hand}")
+            playerScore = analyzeHand(player.hand)
+            if (playerScore > 21):
+                print('Bust!')
+                player.bank = player.bank - bet
+                return
+            if (playerScore == 21):
+                break
+            else:
+                continue
+
+    # Loop for dealer           
+    while True:       
+        print(f"{dealer.name}'s hand: {dealer.hand}")
+
+        dealerScore = analyzeHand(dealer.hand)
+
+        if dealerScore > 21:
+            print('Dealer busts, you win!')
+            player.bank += bet
+            return
+        elif dealerScore > playerScore:
+            print('Better luck next time!')
+            player.bank -= bet
+            return
+        elif dealerScore >= 17: # Dealer will hit if below player's hand and below 17
+            if dealerScore == playerScore:
+                print('Push!')
+                return
+            if dealerScore < playerScore:
+                print('You win!')
+                player.bank += bet
+                return
+            else:
+                print('Better luck next time!')
+                player.bank -= bet
+                return
+        else:
+            dealer.getHand(deck, 1)
